1a076732317a8c8696715b92bdfc4a9bb05536e0
wysiati <sachin.inguva@gmail.com>
2022-10-30 16:33:21 -0400

    US01: Dates before current date US07: Less then 150 years old

--- utils.py
+++ utils.py
@@ -1,15 +1,26 @@
 from datetime import date, timedelta, datetime
 from constants import month_dict
 
+
 def diff_month(d1, d2):
     return (d1.year - d2.year) * 12 + d1.month - d2.month
+
 
 def parseDate(date):
     day, month, year = date.split()
     return datetime(int(year), month_dict[month], int(day))
 
+
 def isDateLess(d1, d2):
     return d1 < d2
+
+
+def isDateLessThanOrEqual(d1, d2):
+    return d1 <= d2
+
+
+def is_not_none(val): return val != "N/A"
+
 
 def thirty_day_difference(this_date):
     d1 = (date.today()-timedelta(days=30))
@@ -20,15 +31,17 @@ def thirty_day_difference(this_date):
         return True
     return False
 
+
 def isDateValid(thisDate):
     if thisDate == "N/A":
         return True
-    day, month, year =  thisDate.split()
+    day, month, year = thisDate.split()
     try:
         d2 = datetime(int(year), month_dict[month], int(day))
         return True
     except ValueError:
         return False
+
 
 def thirty_day_ahead(this_date):
     if this_date == 'N/A':
@@ -41,7 +54,7 @@ def thirty_day_ahead(this_date):
     curr_year = int(curr_year)
     day, month, year = this_date.split()
     d3 = datetime(curr_year, month_dict[month], int(day))
-    curr_year+=1
+    curr_year += 1
     d4 = datetime(curr_year, month_dict[month], int(day))
     if (d3 > d2 and d3 < d1) or (d4 > d2 and d4 < d1):
         return True
